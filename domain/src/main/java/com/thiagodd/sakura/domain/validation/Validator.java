7713fdda38e9d63c67bc19f59edb4fce55804008
Thiago Silva <thiagodd.dev@gmail.com>
2024-03-06 09:15:12 -0300

    refactor: Moved validateField to abstract Validator
          With this, it will be possible to validate attributes in a generic way, avoiding duplication of code.

--- domain/src/main/java/com/thiagodd/sakura/domain/validation/Validator.java
+++ domain/src/main/java/com/thiagodd/sakura/domain/validation/Validator.java
@@ -8,6 +8,16 @@ public abstract class Validator {
         this.handler = handler;
     }
 
+    protected void validateField(String fieldName, String value, int minLength, int maxLength){
+        if (value == null) {
+            this.validationHandler().append(new Error("'" + fieldName + "' should not be null"));
+        } else if (value.isBlank()){
+            this.validationHandler().append(new Error("'" + fieldName + "' should not be empty"));
+        } else if (value.trim().length() < minLength || value.trim().length() > maxLength) {
+            this.validationHandler().append(new Error("'" + fieldName + "' length should be between " + minLength + " and " + maxLength + " characters"));
+        }
+    }
+
     public abstract void validate();
 
     protected ValidationHandler validationHandler(){
