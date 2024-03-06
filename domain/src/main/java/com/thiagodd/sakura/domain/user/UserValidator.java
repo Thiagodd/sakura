5d1ae2d8f02bd334a2d58a54853735e22540ac65
Thiago Silva <thiagodd.dev@gmail.com>
2024-03-06 09:24:14 -0300

    refactor: Revised validateEmailField()
              With this change, the regex will cover different types of emails, and will not be case_sensitive.

--- domain/src/main/java/com/thiagodd/sakura/domain/user/UserValidator.java
+++ domain/src/main/java/com/thiagodd/sakura/domain/user/UserValidator.java
@@ -4,6 +4,9 @@ import com.thiagodd.sakura.domain.validation.Error;
 import com.thiagodd.sakura.domain.validation.ValidationHandler;
 import com.thiagodd.sakura.domain.validation.Validator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserValidator extends Validator {
 
     private static final int NAME_MAX_LENGTH = 255;
@@ -12,8 +15,10 @@ public class UserValidator extends Validator {
     private static final int LOGIN_MIN_LENGTH = 3;
     private static final int EMAIL_MAX_LENGTH = 255;
     private static final int EMAIL_MIN_LENGTH = 3;
+    private static final String EMAIL_REGEX = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)])";
 
-    private User user;
+
+    private final User user;
 
     public UserValidator(final User user, final ValidationHandler handler) {
         super(handler);
@@ -46,9 +51,12 @@ public class UserValidator extends Validator {
         validateLoginField();
     }
 
-    private void validateEmailField(){
+    private void validateEmailField() {
         final var email = user.getEmail();
-        if (email.matches("^[\\w\\.-]+@[a-zA-Z\\d\\.-]+\\.[a-zA-Z]{2,}$")){
+        Pattern pattern = Pattern.compile(EMAIL_REGEX, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(email);
+
+        if (!matcher.matches()) {
             this.validationHandler().append(new Error("'email' is in an invalid format"));
         }
     }
